3d99252d4c68d24fcc4a22a3710e312723fb5ca7
Himanshu <57253980+himanshu07121999@users.noreply.github.com>
2021-11-29 21:59:11 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,36 @@
+ /*const firebaseConfig = {
+    apiKey: "AIzaSyBNeeTKfj6dArt8qtS3Wf6FlR_WjCkFOys",
+    authDomain: "dummy-4763f.firebaseapp.com",
+    databaseURL: "https://dummy-4763f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "dummy-4763f",
+    storageBucket: "dummy-4763f.appspot.com",
+    messagingSenderId: "900395298163",
+    appId: "1:900395298163:web:71bcadb4908e1ec3656906",
+    measurementId: "G-SJL4E53H75"
+  };
+//const auth=firebase.auth();
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+*/
+/*
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+   firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  })
+  });
+*/
---
+++ signOut.js
@@ -0,0 +1,8 @@
+/*function signOut(googleUser) {
+
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function (googleUser) {
+      console.log('User signed out.');
+    });
+  }
+*/
